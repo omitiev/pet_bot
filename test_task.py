3df3d6117c1c5f0a284bb8f4eb18ba8d6dc8a1c5
oleksii <oleksii.mitiev@gmail.com>
2017-07-10 00:23:14 +0300

    fixed division by zero bug

--- test_task.py
+++ test_task.py
@@ -4,5 +4,9 @@ print ("=========================")
 
 a=10
 b=2
-c=a/b
-print ("Result of a/b (a%d, b=%d) is %.2f" % (a, b, c))
+
+if b!=0:
+    c=a/b
+    print ("Result of a/b (a%d, b=%d) is %.2f" % (a, b, c))
+else:
+    print ("Error: Division by zero")
