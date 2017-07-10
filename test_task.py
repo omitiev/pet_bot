d7f7605a16962d7240906a143cc89c3c3513399d
oleksii <oleksii.mitiev@gmail.com>
2017-07-10 23:35:38 +0300

    fixed division by zero bug

--- test_task.py
+++ test_task.py
@@ -1,12 +1,79 @@
-print ("=========================")
+import math
+
+'''
+print ("==========================================================")
 print ("test task")
-print ("=========================")
+print ("==========================================================")
 
 a=10
 b=2
 
 if b!=0:
     c=a/b
-    print ("Result of a/b (a%d, b=%d) is %.2f" % (a, b, c))
+    print ("Result of a/b (a=%d, b=%d) is %.2f" % (a, b, c))       - how this row works? please explain on the lesson
 else:
     print ("Error: Division by zero")
+'''
+
+
+
+'''
+print ("==========================================================")
+print ("task 1")
+print ("==========================================================")
+
+a = float(input("Please, enter a namber"))
+print ("variable a is:", a)
+b = float(input("Please, enter a namber"))
+print ("variable b is:", b)
+c = float(input("Please, enter a namber"))
+print ("variable c is:", c)
+x=a+b*(c/2)
+print ("Result of expression a+b*(c/2) is:" , x)
+'''
+
+
+
+print ("==========================================================")
+print ("task 2")
+print ("==========================================================")
+
+a = float(input("Please, enter a namber"))
+print ("variable a is:", a)
+b = float(input("Please, enter a namber"))
+print ("variable b is:", b)
+x=(a**2+b**2)%2
+print ("Result of expression (a**2+b**2)%2 is:" , x)
+
+
+
+print ("==========================================================")
+print ("task 3")
+print ("==========================================================")
+
+
+
+
+
+
+print ("==========================================================")
+print ("task 4")
+print ("==========================================================")
+
+
+
+
+
+
+print ("==========================================================")
+print ("task 5")
+print ("==========================================================")
+
+
+
+
+
+
+print ("==========================================================")
+print ("task 6")
+print ("==========================================================")
