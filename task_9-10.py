f1c012936df6e74e0a08fdc0a803a8e141522587
oleksii <oleksii.mitiev@gmail.com>
2017-07-16 16:08:50 +0300

    removed mistake in English word

--- task_9-10.py
+++ task_9-10.py
@@ -13,15 +13,30 @@ print("As you can see your text contains %.d words" % index)
 word = int(input("Please select the serial number of word which you want to make in UPPER case:"))
 if word>=1 and index >= word:
     print("OK, I\'ll change for you the word #%.d" % word)
-    lst_1[word-1] = str.upper(str(lst_1[word-1]))
+    lst_1[word-1] = lst_1[word-1].upper()
+    str.upper(str(lst_1[word - 1]))
     print("After some changes your text looks like that:\n"," ".join(lst_1))
 else:
     print("Your text doesn\'t have such word")
 
-
-
-
-
 # Дана строка вида "Leo Tolstoy*1828-08-28*1910-11-20". В этой строке указаны имя писателя и через символ * даты рождения и смерти.
 # Даты указаны в формате "YYYY-MM-DD". Требуется написать программу, которая по переданной строке определит возраст писателя и вернет его имя и возраст.
 # Например, для строки "Leo Tolstoy*1828-08-28*1910-11-20" программа должна вернуть: "Leo Tolstoy", 82. Месяцы и дни можно игнорировать.
+
+print ("==========================================================")
+print ("task 10")
+print ("==========================================================")
+
+cond = str(input("Please, enter condition for the task in the next way name*YYYY-MM-DD*YYYY-MM-DD : "))
+print("You entered next condition : \n%s" % cond)
+lst_1 = cond.split('*')
+lst1_1 = lst_1[1].split('-')
+lst1_2 = lst_1[1].split('-')
+print(lst_1)
+
+if word>=1 and index >= word:
+    print("OK, I\'ll change for you the word #%.d" % word)
+    lst_1[word-1] = str.upper(str(lst_1[word-1])) # I think this row could be improved in some way without transfer to a string
+    print("After some changes your text looks like that:\n"," ".join(lst_1))
+else:
+    print("Your text doesn\'t have such word")
