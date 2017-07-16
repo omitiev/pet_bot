93a8e4fcba426d05f638b8a23db497ab0322e560
oleksii <oleksii.mitiev@gmail.com>
2017-07-16 23:12:40 +0300

    intermediate commit task 11 - done task 12 - NO task 13 - NO task 14 - NO

--- task_9-10.py
+++ task_9-10.py
@@ -1,9 +1,9 @@
 # Написать программу, которая переводит в верхний регистр второе слово (слово - последовательность символов между двумя пробелами).
 # Например: “abc def ghj” -> “abc DEF ghj”
 
-print ("==========================================================")
-print ("task 9")
-print ("==========================================================")
+print("==========================================================")
+print("task 9")
+print("==========================================================")
 
 str_1 = str(input("Please, enter any text: "))
 print("Thanks! Your texts are: \n%s" % str_1)
@@ -11,11 +11,11 @@ lst_1 = str.split(str_1)
 index = int(len(lst_1))
 print("As you can see your text contains %.d words" % index)
 word = int(input("Please select the serial number of word which you want to make in UPPER case:"))
-if word>=1 and index >= word:
+if word >= 1 and index >= word:
     print("OK, I\'ll change for you the word #%.d" % word)
-    lst_1[word-1] = lst_1[word-1].upper()
+    lst_1[word - 1] = lst_1[word - 1].upper()
     str.upper(str(lst_1[word - 1]))
-    print("After some changes your text looks like that:\n"," ".join(lst_1))
+    print("After some changes your text looks like that:\n", " ".join(lst_1))
 else:
     print("Your text doesn\'t have such word")
 
@@ -23,16 +23,17 @@ else:
 # Даты указаны в формате "YYYY-MM-DD". Требуется написать программу, которая по переданной строке определит возраст писателя и вернет его имя и возраст.
 # Например, для строки "Leo Tolstoy*1828-08-28*1910-11-20" программа должна вернуть: "Leo Tolstoy", 82. Месяцы и дни можно игнорировать.
 
-print ("==========================================================")
-print ("task 10")
-print ("==========================================================")
+print("==========================================================")
+print("task 10")
+print("==========================================================")
 
-cond = str(input("Please, enter condition for the task in the next way name*birth date*death date (date should be in the next format YYYY-MM-DD): "))
+cond = str(input(
+    "Please, enter condition for the task in the next way name*birth date*death date (date should be in the next format YYYY-MM-DD): "))
 print("You entered next condition : \n%s" % cond)
 lst_1 = cond.split('*')
 lst_1[1] = lst_1[1].split('-')
 lst_1[2] = lst_1[2].split('-')
-age = int(lst_1[2][0])-int(lst_1[1][0])
+age = int(lst_1[2][0]) - int(lst_1[1][0])
 print()
 if age >= 0:
     print("I can make a conclusion that " + lst_1[0] + " died at %d" % age)
