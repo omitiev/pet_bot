791780b7b611ba295626f1f3f0d43644f6c2a46b
oleksii <oleksii.mitiev@gmail.com>
2017-07-17 09:15:44 +0300

    intermediate commit task 11 - done (code improvement) task 12 - NO task 13 - NO task 14 - NO

--- task_11-14.py
+++ task_11-14.py
@@ -11,18 +11,26 @@ def find_rad(x):
 def find_cos(x):
     cos = math.cos(x)
     return cos
+d_sym = '\u00b0'
+print(d_sym)
 
 d_angle_1 = int(input("Enter the value in degree for angle 1: "))
 d_angle_2 = int(input("Enter the value in degree for angle 2: "))
 d_angle_3 = int(input("Enter the value in degree for angle 3: "))
-print("Now we will find the value of cosine for corresponding angles")
-r_angle_1 = find_rad(d_angle_1)
-cos_angle_1 = find_cos(r_angle_1)
-r_angle_2 = find_rad(d_angle_2)
-cos_angle_2 = find_cos(r_angle_2)
-r_angle_3 = find_rad(d_angle_3)
-cos_angle_3 = find_cos(r_angle_3)
+print("Now we will find the value of cosine for corresponding angles: %d%s , %d%s , %d%s" % (d_angle_1, d_sym, d_angle_2, d_sym, d_angle_3, d_sym))
+cos_angle_1 = find_cos(find_rad(d_angle_1))
+cos_angle_2 = find_cos(find_rad(d_angle_2))
+cos_angle_3 = find_cos(find_rad(d_angle_3))
 print("If I'm right the answers are: \n %.16f for angle 1 \n %.16f for angle 2 \n %.16f for angle 3" % (cos_angle_1,cos_angle_2, cos_angle_3))
+
+# r_angle_1 = find_rad(d_angle_1)
+# cos_angle_1 = find_cos(r_angle_1)
+# r_angle_2 = find_rad(d_angle_2)
+# cos_angle_2 = find_cos(r_angle_2)
+# r_angle_3 = find_rad(d_angle_3)
+# cos_angle_3 = find_cos(r_angle_3)
+#
+# print("If I'm right the answers are: \n %.16f for angle 1 \n %.16f for angle 2 \n %.16f for angle 3" % (cos_angle_1,cos_angle_2, cos_angle_3))
 
 # Написать функцию, которая рассчитывает сумму всех цифр некоторого трехзначного числа,
 # введенного пользователем в консоли, без использования операторов цикла.
