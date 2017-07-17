1cad1b725e81ccf4fa2bef14e89a533fd4378a14
oleksii <oleksii.mitiev@gmail.com>
2017-07-17 17:45:44 +0300

    changes in task 8 - task 8 done fully

--- task_7-8.py
+++ task_7-8.py
@@ -10,7 +10,9 @@ lst1=us_date.split(".")
 eu_date = str(lst1[1])+'.'+str(lst1[0])+'.'+str(lst1[2])
 print ("In EU format the date will be", eu_date)
 
-#Написать программу, которая берет две строки и помещает первую строку в середину второй. Результат помещается в середину первой. Длину строки можно получить с помощью функции len().
+# Написать программу, которая берет две строки и помещает первую строку в середину второй.
+# Результат помещается в середину первой.
+# Длину строки можно получить с помощью функции len().
 
 print ("==========================================================")
 print ("task 8")
@@ -18,10 +20,14 @@ print ("==========================================================")
 
 str1 = str(input("Please, enter any text: "))
 str2 = str(input("OK, now enter any other text: "))
-print ("Thanks! Your texts are: \n1.", str1, "\n2.", str2)
-index=int(round(len(str2))/2)
-str2_1=str2[:index]
-str2_2=str2[index:]
-new_str=str2_1+str1+str2_2
-print("But I have improved your texts and now they look like a single text - see below:\n",new_str)
+print("Thanks! Your texts are: \n1.", str1, "\n2.", str2)
+index_str1 = int(round(len(str1))/2)
+str1_1 = str1[:index_str1]
+str1_2 = str1[index_str1:]
+index_str2 = int(round(len(str2))/2)
+str2_1 = str2[:index_str2]
+str2_2 = str2[index_str2:]
+new_str2 = str2_1+str1+str2_2
+new_str1 = str1_1 + new_str2 + str1_2
+print("But I have improved your texts and now they look like a single text - see below:\n", new_str1)
 
