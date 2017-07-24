aefded3eaa887da33dc8f303bd603fc61e42e3f4
oleksii <oleksii.mitiev@gmail.com>
2017-07-24 23:44:27 +0300

    task 15 - added case when one circle inside the other

--- task_15.py
+++ task_15.py
@@ -7,9 +7,11 @@ import default
 
 default.start(15)
 
-def is_crossing(radiuses, distance):
-    if radiuses <= distance:
+def is_crossing(radius_1, radius_2, distance):
+    if (radius_1 + radius_2) <= distance:
         print("The circles with corresponding coordinates and radiuses don't cross")
+    elif ((radius_1 > radius_2) and radius_1 > radius_2 +distance) or ((radius_2 > radius_1) and radius_2 > radius_1 +distance):
+        print("The circles with corresponding coordinates and radiuses don't cross (one circle inside the other)")
     else:
         print("The circles with corresponding coordinates and radiuses cross")
 
@@ -24,8 +26,7 @@ circle_2_x = int(circle_coordinates_2_lst[0])
 circle_2_y = int(circle_coordinates_2_lst[1])
 radius_2 = int(circle_coordinates_2_lst[2])
 dist_btw_circles = math.sqrt((circle_1_x - circle_2_x)**2 + (circle_1_y - circle_2_y)**2)
-r_sum = radius_1 + radius_2
 
-is_crossing(r_sum, dist_btw_circles)
+is_crossing(radius_1, radius_2, dist_btw_circles)
 
 default.end()
