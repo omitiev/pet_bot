f3978b8b2565aa4104e26a2741fad2738cba550e
oleksii <oleksii.mitiev@gmail.com>
2017-07-26 12:17:40 +0300

    task 15 - added case when one circle inside the other

---
+++ extra_task_1.py
@@ -0,0 +1,13 @@
+import default
+
+default.start(1)
+
+
+def reverse_str(string):
+    return string[::-1]
+
+s_1 = input("Please enter the string for reverse:")
+s_2 = reverse_str(s_1)
+print("Have a look at new string: %s" % s_2)
+
+default.end()
