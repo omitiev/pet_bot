ed24e93608f92c43b69e72825a887bb9c4ec969d
oleksii <oleksii.mitiev@gmail.com>
2017-08-05 17:17:04 +0300

    task 28 - done

---
+++ tast_29.py
@@ -0,0 +1,27 @@
+# Создайте список из 11 случайных целых чисел из отрезка [-1;1], выведите список на экран в строку.
+# Определите какой элемент встречается в списке чаще всего и выведите об этом сообщение на экран.
+# Если два каких-то элемента встречаются одинаковое количество раз, то не выводить ничего.
+
+
+import default
+import random
+
+
+default.start(29)
+
+
+lst1 = []
+[lst1.append(random.randint(-1, 1)) for i in range(11)]
+print("The strange string: %s" % (", ".join([str(i) for i in lst1])))
+if (lst1.count(-1) > lst1.count(0)) or (lst1.count(-1) > lst1.count(1)):
+    print("The most popular number in the list is -1")
+elif (lst1.count(0) > lst1.count(-1)) or (lst1.count(0) > lst1.count(1)):
+    print("The most popular number in the list is 0")
+elif (lst1.count(1) > lst1.count(-1)) or (lst1.count(1) > lst1.count(0)):
+    print("The most popular number in the list is 0")
+else:
+    print()
+
+
+default.end()
+
