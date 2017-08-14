0931a25b3ab6e7b086aa2d7a2842d65b47abed19
oleksii <oleksii.mitiev@gmail.com>
2017-08-14 20:57:10 +0300

    quiz tasks 7 - done

---
+++ quiz_tasks/quiz_task_8.py
@@ -0,0 +1,22 @@
+# 8. В одномерном массиве поменять местами минимальный и максимальный элементы. Остальные оставить на своих местах.
+
+import default
+import copy
+
+default.start(7)
+
+
+def replace_min_max_values(lst1):
+    lst2 = copy.deepcopy(lst1)
+    lst2[lst1.index(max(lst1))] = min(lst1)
+    lst2[lst1.index(min(lst1))] = max(lst1)
+    return lst2
+
+
+base_lst = default.select_random(default.primes(1000), 10)
+print("Base lst is: ", base_lst)
+base_lst = replace_min_max_values(base_lst)
+print("The new lst is: ", base_lst)
+
+
+default.end()
