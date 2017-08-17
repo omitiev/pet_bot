648f60b9c8e8df870aea71fdce1a2766fba9f8d8
oleksii <oleksii.mitiev@gmail.com>
2017-08-17 17:15:23 +0300

    quiz tasks 4 - done

--- quiz_tasks/quiz_task_4.py
+++ quiz_tasks/quiz_task_4.py
@@ -13,18 +13,20 @@ def multipy_odds(number):
         for i in nums:
             if default.is_odd(int(i)):
                 x *= int(i)
-        if x != 1:
+        if x > 1:
             return x
+        elif x == 1 and '1' in number:
+            return 1
         else:
             return 0
 
 number = str(input("Please enter a number with 5 digits: "))
 if len(number) == 5:
     print("Your number is:", number)
-    if multipy_odds(number) != 0:
+    if multipy_odds(number) >= 1:
         print("The multiplication of all odd digits in number is %d" % (multipy_odds(number)))
     else:
-        print("Your number doesn't contain odd digits or multiplication of all odd digits = 1")
+        print("Your number doesn't contain odd digits")
 else:
     print("Something went wrong, please observe your number")
 
