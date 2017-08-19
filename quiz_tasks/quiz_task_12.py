e97a2feb8823e4405b204aeca0d61454df4eabf6
oleksii <oleksii.mitiev@gmail.com>
2017-08-19 14:37:53 +0300

    quiz tasks 12 - done

--- quiz_tasks/quiz_task_12.py
+++ quiz_tasks/quiz_task_12.py
@@ -12,9 +12,27 @@ import random
 default.start(12)
 
 
+def create_list_with_tasks():
+    tasks_dict={}
+    for i in range(2, 10):
+        for j in range(2, 10):
+            task = str(i)+'*'+str(j)
+            result = i*j
+            tasks_dict[task] = result
+    return tasks_dict
 
 
+def select_random_tasks(all, ammount):
+    full_list_of_tasks = list(all.keys())
+    list_of_tasks = []
+    while len(set(list_of_tasks)) < ammount:
+        list_of_tasks.append(random.choice(full_list_of_tasks))
+    return set(list_of_tasks)
 
+
+f_tasks = create_list_with_tasks()
+rand_tasks = select_random_tasks(f_tasks, 15)
+print("Today we should decide next tasks: ", rand_tasks)
 
 
 default.end()
